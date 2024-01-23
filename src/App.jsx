3f4c991ebd6bb2e6b3dfa3c3a3338f1cd43ddf92
DevNik14 <nykyurchiyski@gmail.com>
2024-01-23 21:02:56 +0200

    refactor: remove MUI nav link text and add custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import SiteNavigation from './components/Navbar/SiteNavigation'
 import './App.css';
+import { Container } from '@mui/material';
 
 function App() {
 
   return (
-    <SiteNavigation/>
+    <Container>
+      <SiteNavigation/>
+    </Container>
   )
 }
 
