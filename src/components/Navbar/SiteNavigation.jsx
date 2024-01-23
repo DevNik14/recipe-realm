9e218d1a70b16f59a32597351df8fc7a151b4443
DevNik14 <nykyurchiyski@gmail.com>
2024-01-23 22:54:30 +0200

    refactor: make site nav items render dynamically

--- src/components/Navbar/SiteNavigation.jsx
+++ src/components/Navbar/SiteNavigation.jsx
@@ -3,7 +3,13 @@ const navItems = ['Home', 'Login'];
 function SiteNavigation() {
   return (
     <nav className="site-navigation">
-      
+      <ul className="site-navigation-items">
+        {
+          navItems.map(item => {
+            return <li className="site-navigation-item" key={item}>{item}</li>
+          })
+        }
+      </ul>
     </nav>
   )
 }
