5d0868764f23d80363da8ce44a8b08575d67bd8e
DevNik14 <nykyurchiyski@gmail.com>
2024-01-24 13:29:44 +0200

    style: add css reset file and styles

--- src/components/Navbar/SiteNavigation.jsx
+++ src/components/Navbar/SiteNavigation.jsx
@@ -5,7 +5,7 @@ const navItems = ['Home', 'Login'];
 function SiteNavigation() {
   return (
     <nav className="site-navigation">
-      <ul className={`${styles.navbar} site-navigation-items`}>
+      <ul className={`site-navigation-items`}>
         {
           navItems.map(item => {
             return <li className="site-navigation-item" key={item}>{item}</li>
