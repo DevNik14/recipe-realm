0542f2e45d30c3f1b204cbe12266055002815bd0
DevNik14 <nykyurchiyski@gmail.com>
2024-01-24 15:40:23 +0200

    add content to the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,11 @@
 const Hero = () => {
   return (
-    <header>
-      <h1>Discover, Share, and Enjoy Delicious Recipes</h1>
+    <header className="hero-section">
+      <div className="hero-content">
+        <h1>Discover, Share, and Enjoy Delicious Recipes</h1>
+        <p>Welcome to Recipe Realm, your go-to destination for culinary inspiration and delightful recipes.</p>
+        <p>Explore a world of flavors, connect with fellow food enthusiasts, and make every meal a memorable experience.</p>
+      </div>
     </header>
   )
 }
