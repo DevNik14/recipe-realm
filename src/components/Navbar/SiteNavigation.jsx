0b2455c203202a1c1f1524158175cc934d588444
DevNik14 <nykyurchiyski@gmail.com>
2024-01-26 21:51:18 +0200

    style: add mobile navigation icon and make it toggable

--- src/components/Navbar/SiteNavigation.jsx
+++ src/components/Navbar/SiteNavigation.jsx
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 import styles from './Navbar.module.css'
 
-const navItems = ['Home', 'Recipes', 'Blog', 'Login'];
-
-const displayNavLinks = (from, to) => {
-  return navItems.slice(from, to).map(item => {
-    return <li className={styles.siteNavigationItem} key={item}>{item}</li>
-  })
-}
-
 function SiteNavigation() {
+  const [toggleMobileNavigation, setToggleMobileNavigation] = useState(false);
+  const navItems = ['Home', 'Recipes', 'Blog', 'Login'];
+
+  const displayNavLinks = (from, to) => {
+    return navItems.slice(from, to).map(item => {
+      return <li className={styles.siteNavigationItem} key={item}>{item}</li>
+    })
+  }
+
+  const toggleMobileNavigationHandler = () => {
+    setToggleMobileNavigation(oldState => !oldState)
+  }
+
   return (
     <nav className={styles.siteNavigation}>
-      <ul className={styles.siteNavigationItems}>
-        <div className="left">
+      <ul className={`${styles.siteNavigationItems}`}>
+        <div className={styles.left}>
           <li className={styles.siteNavigationItem}>Recipe Realm</li>
         </div>
         <div className={styles.middle}>
           {displayNavLinks(0, navItems.length - 1)}
         </div>
-        <div className="right">
+        <div className={styles.right}>
           {displayNavLinks(3)}
+        </div>
+        <div className={`${styles.mobileMenuIcon} ${toggleMobileNavigation && styles.open}`} onClick={toggleMobileNavigationHandler}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </ul>
     </nav>
