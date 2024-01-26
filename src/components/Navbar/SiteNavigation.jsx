022d7dbdf25c69d618600eb22f0004530f790998
DevNik14 <nykyurchiyski@gmail.com>
2024-01-26 19:17:23 +0200

    feat(nav): implement dynamic navigation links for flexible positioning

--- src/components/Navbar/SiteNavigation.jsx
+++ src/components/Navbar/SiteNavigation.jsx
@@ -1,16 +1,26 @@
 import styles from './Navbar.module.css'
 
-const navItems = ['Home', 'Recipes', 'Login'];
+const navItems = ['Home', 'Recipes', 'Blog', 'Login'];
+
+const displayNavLinks = (from, to) => {
+  return navItems.slice(from, to).map(item => {
+    return <li className="site-navigation-item" key={item}>{item}</li>
+  })
+}
 
 function SiteNavigation() {
   return (
-    <nav className="site-navigation">
+    <nav className={styles.siteNavigation}>
       <ul className={`site-navigation-items`}>
-        {
-          navItems.map(item => {
-            return <li className="site-navigation-item" key={item}>{item}</li>
-          })
-        }
+        <div className="left">
+          <li className="site-navigation-item">Recipe Realm</li>
+        </div>
+        <div className="middle">
+          {displayNavLinks(0, navItems.length - 1)}
+        </div>
+        <div className="right">
+          {displayNavLinks(3)}
+        </div>
       </ul>
     </nav>
   )
