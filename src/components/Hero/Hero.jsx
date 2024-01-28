e44a0ad21db18c2ce443fc5d105b7881aa29d0df
DevNik14 <nykyurchiyski@gmail.com>
2024-01-28 20:04:49 +0200

    style: give hero section basic styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,11 +4,15 @@ const Hero = () => {
   return (
     <header className={styles.heroSection}>
       <div className={styles.heroContent}>
-        <h1>Discover, Share, and Enjoy Delicious Recipes</h1>
-        <p>Welcome to Recipe Realm, your go-to destination for culinary inspiration and delightful recipes.</p>
-        <p>Explore a world of flavors, connect with fellow food enthusiasts, and make every meal a memorable experience.</p>
+        <div>
+          <h1>Discover, Share, and Enjoy Delicious Recipes</h1>
+        </div>
+        <div>
+          <p>Welcome to Recipe Realm, your go-to destination for culinary inspiration and delightful recipes.
+            Explore a world of flavors, connect with fellow food enthusiasts, and make every meal a memorable experience.
+          </p>
+        </div>
         <div className={styles.heroCtaButtons}>
-          <button>Get Started</button>
           <button>Explore Recipes</button>
         </div>
       </div>
