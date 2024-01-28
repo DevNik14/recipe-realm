cb6c4907049fe7e5620d41e6441f58bc2983f163
DevNik14 <nykyurchiyski@gmail.com>
2024-01-28 19:39:51 +0200

    refactor: make the classNames for the hero section Dynamic

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
+import styles from './Hero.module.css'
+
 const Hero = () => {
   return (
-    <header className="hero-section">
-      <div className="hero-content">
+    <header className={styles.heroSection}>
+      <div className={styles.heroContent}>
         <h1>Discover, Share, and Enjoy Delicious Recipes</h1>
         <p>Welcome to Recipe Realm, your go-to destination for culinary inspiration and delightful recipes.</p>
         <p>Explore a world of flavors, connect with fellow food enthusiasts, and make every meal a memorable experience.</p>
-        <div className="hero-cta-buttons">
+        <div className={styles.heroCtaButtons}>
           <button>Get Started</button>
           <button>Explore Recipes</button>
         </div>
