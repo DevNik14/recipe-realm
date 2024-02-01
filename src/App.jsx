4da031d0d99748cc889290294cf30b87422711ac
DevNik14 <nykyurchiyski@gmail.com>
2024-02-01 17:22:00 +0200

    refactor: move the home page content into a home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import {Routes, Route} from 'react-router-dom';
-import SiteNavigation from './components/Navbar/SiteNavigation';
-import Hero from './components/Hero/Hero';
-import TrendingRecipes from './components/TrendingRecipes/TrendingRecipes';
-import HomeSearch from './components/HomeSearch/HomeSearch';
-import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/recipes' element={<h2>Recipes</h2>}/>
-      <Route path='/blog' element={<h2>Blog</h2>}/>
-    </Routes>
-      <SiteNavigation />
-      <Hero />
-      <HomeSearch />
-      <TrendingRecipes />
-      <Footer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipes' element={<h2>Recipes</h2>} />
+        <Route path='/blog' element={<h2>Blog</h2>} />
+      </Routes>
+      <Home />
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import SiteNavigation from '../Navbar/SiteNavigation';
+import Hero from '../Hero/Hero';
+import TrendingRecipes from '../TrendingRecipes/TrendingRecipes';
+import HomeSearch from '../HomeSearch/HomeSearch';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <SiteNavigation />
+      <Hero />
+      <HomeSearch />
+      <TrendingRecipes />
+      <Footer />
+    </>
+  )
+}
+
+export default Home;
