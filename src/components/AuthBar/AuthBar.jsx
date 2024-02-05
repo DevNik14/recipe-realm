7051d5f3c216b147ecbbe211dce65f9c74c90ffd
DevNik14 <nykyurchiyski@gmail.com>
2024-02-05 20:36:37 +0200

    style: add basic styling to the auth nav items

--- src/components/AuthBar/AuthBar.jsx
+++ src/components/AuthBar/AuthBar.jsx
@@ -4,8 +4,8 @@ import { Nav } from 'react-bootstrap';
 const AuthBar = () => {
   return (
     <Nav className={`${styles.authNav} auth justify-content-end`}>
-      <Nav.Item className={`${styles.loginButton} ${styles.authItem}`}>Login</Nav.Item>
-      <Nav.Item className={`${styles.authItem}`}>Sign Up</Nav.Item>
+      <Nav.Item className={`${styles.loginButton} ${styles.authItem}`}><span>Login</span></Nav.Item>
+      <Nav.Item className={`${styles.authItem}`}><span>Sign Up</span></Nav.Item>
     </Nav>
   )
 }
