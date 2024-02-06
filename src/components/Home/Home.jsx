69653df8cb81d9cda98fe3dab3b1df25c1704b9b
DevNik14 <nykyurchiyski@gmail.com>
2024-02-06 18:20:03 +0200

    refactor: add Bootstrap Form component instead of custom one

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,17 @@ import Hero from '../Hero/Hero';
 import TrendingRecipes from '../TrendingRecipes/TrendingRecipes';
 import HomeSearch from '../HomeSearch/HomeSearch';
 import Footer from '../Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <>
-      <Hero />
-      <HomeSearch />
-      <TrendingRecipes />
-      <Footer />
+      <Container>
+        <Hero />
+        <HomeSearch />
+        <TrendingRecipes />
+        <Footer />
+      </Container>
     </>
   )
 }
--- src/components/HomeSearch/HomeSearch.jsx
+++ src/components/HomeSearch/HomeSearch.jsx
@@ -1,12 +1,19 @@
 import styles from './HomeSearch.module.css'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const HomeSearch = () => {
   return (
     <div className={`${styles.homeSearch}`}>
-      <form className={styles.homeSearchForm}>
-        <input type="text" placeholder="Search for a specific recipes" />
-        <input type="button" value="Search" />
-      </form>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Button variant="primary">
+          Search
+        </Button>
+      </Form>
     </div>
   )
 }
