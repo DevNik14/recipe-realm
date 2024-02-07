6df68166231298b753719e0435ca248f98b75714
DevNik14 <nykyurchiyski@gmail.com>
2024-02-07 19:12:44 +0200

    refactor: combine login and sign up button

--- src/components/AuthBar/AuthBar.jsx
+++ src/components/AuthBar/AuthBar.jsx
@@ -4,8 +4,7 @@ import { Nav } from 'react-bootstrap';
 const AuthBar = () => {
   return (
     <Nav className={`${styles.authNav} auth justify-content-end`}>
-      <Nav.Item className={`${styles.loginButton} ${styles.authItem}`}><span>Login</span></Nav.Item>
-      <Nav.Item className={`${styles.authItem}`}><span>Sign Up</span></Nav.Item>
+      <Nav.Item className={`${styles.loginButton} ${styles.authItem}`}><span>Login / Sign Up</span></Nav.Item>
     </Nav>
   )
 }
--- src/components/TrendingRecipes/TrendingRecipes.jsx
+++ src/components/TrendingRecipes/TrendingRecipes.jsx
@@ -4,12 +4,13 @@ import styles from './TrendingRecipes.module.css';
 const recipes = [
   { title: "Recipe Title", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
   { title: "Recipe Title", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+  { title: "Recipe Title", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
   { title: "Recipe Title", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" }
 ]
 
 const TrendingRecipes = () => {
   return (
-    <section className={`${styles.trendingRecipesHomeSection} maxWdithContainer`}>
+    <section className={`${styles.trendingRecipesHomeSection} justify-content-center`}>
       <div className={styles.trendingRecipesContent}>
         <h2>Trending Recipes</h2>
         {
