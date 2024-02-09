41b26334ad35f45f066c85d11b85e34023830b19
DevNik14 <nykyurchiyski@gmail.com>
2024-02-09 21:09:17 +0200

    refactor: move menu items into a seperate Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import styles from './Menu.module.css';
+
+const Menu = () => {
+  const navItems = ['Home', 'Recipes', 'Blog'];
+  const transformHomeLink = (linkText) => linkText == 'Home' ? '/' : linkText;
+
+  const displayNavLinks = (from, to) => {
+    return navItems.slice(from, to).map(item => {
+      return <Nav.Item key={item}>
+        <NavLink to={transformHomeLink(item)} className={styles.siteNavigationItem}>{item}</NavLink>
+      </Nav.Item>
+    })
+  }
+
+  return (
+    <Navbar collapseOnSelect expand="lg">
+    <Navbar.Brand>Recipe Realm</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+      <Nav>
+        {displayNavLinks(0)}
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  )
+}
+
+export default Menu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,14 @@
-import { NavLink } from 'react-router-dom';
 import AuthBar from '../AuthBar/AuthBar';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import styles from './Navbar.module.css'
+import Menu from '../Menu/Menu'; 
 
 function SiteNavigation() {
-  const navItems = ['Home', 'Recipes', 'Blog'];
-  const transformHomeLink = (linkText) => linkText == 'Home' ? '/' : linkText;
-
-  const displayNavLinks = (from, to) => {
-    return navItems.slice(from, to).map(item => {
-      return <Nav.Item key={item}>
-        <NavLink to={transformHomeLink(item)} className={styles.siteNavigationItem}>{item}</NavLink>
-      </Nav.Item>
-    })
-  }
-
   return (
     <header className={styles.siteNavigation}>
       <Container>
-        <AuthBar/>
-        <Navbar collapseOnSelect expand="lg">
-          <Navbar.Brand>Recipe Realm</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <Nav>
-              {displayNavLinks(0)}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <AuthBar />
+        <Menu />
       </Container>
     </header>
   )
