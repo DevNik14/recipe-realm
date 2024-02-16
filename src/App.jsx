77d1a9e844c39ca8757d737e1ba76dea2a65a6b5
DevNik14 <nykyurchiyski@gmail.com>
2024-02-16 22:24:10 +0200

    refactor: lift the recipes to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Recipe from './components/Recipe/Recipe';
 
 function App() {
+  const recipes = [
+    { id: 1, title: "Recipe Title 1", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 2, title: "Recipe Title 2", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 3, title: "Recipe Title 3", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 4, title: "Recipe Title 4", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" }
+  ]
 
   return (
     <>
       <SiteNavigation />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/recipes' element={<Recipes />} />
+        <Route path='/' element={<Home recipes={recipes}/>} />
+        <Route path='/recipes' element={<Recipes recipes={recipes}/>} />
         <Route path='/recipes/:recipeId' element={<Recipe />} />
         <Route path='/blog' element={<Blog />} />
         <Route path='/auth' element={<Auth/>} />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,14 +4,7 @@ import HomeSearch from '../HomeSearch/HomeSearch';
 import Footer from '../Footer/Footer';
 import { Container } from 'react-bootstrap';
 
-const Home = () => {
-  const recipes = [
-    { id: 1, title: "Recipe Title 1", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-    { id: 2, title: "Recipe Title 2", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-    { id: 3, title: "Recipe Title 3", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-    { id: 4, title: "Recipe Title 4", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" }
-  ]
-
+const Home = ({recipes}) => {
   return (
     <>
       <Container>
