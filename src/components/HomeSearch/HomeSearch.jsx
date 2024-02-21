ad922a7c08837396620a03a3c0c80d95b9e18cf9
DevNik14 <coyg1414@gmail.com>
2024-02-21 14:13:58 +0200

    refactor; center the home input search

--- src/components/HomeSearch/HomeSearch.jsx
+++ src/components/HomeSearch/HomeSearch.jsx
@@ -3,14 +3,14 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const HomeSearch = () => {
   return (
-    <section className={`${styles.homeSearch}`}>
-      <Form.Group className="mb-3" as={Col} lg={6}>
-        <Form.Control type="email" placeholder="Search through all recipes" />
+    <Row className={`${styles.homeSearch} justify-content-center`}>
+      <Form.Group as={Col} lg={6}>
+        <Form.Control className="mb-3 rounded-pill" type="email" placeholder="Search through all recipes" />
         <Button variant="primary">
           Search
         </Button>
       </Form.Group>
-    </section>
+    </Row>
   )
 }
 
