3384b5651a0dc0cd17844163fea4de7e17614afa
DevNik14 <nykyurchiyski@gmail.com>
2024-03-25 20:55:16 +0200

    test: switch back to dummy data for testing purpose

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import UserProfile from './components/UserProfile/UserProfile';
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:3030/data/recipes')
-      .then(res => res.json())
-      .then(data => {
-        setRecipes(data);
-      })
-  }, []);
+  // const [recipes, setRecipes] = useState([]);
+  // useEffect(() => {
+  //   fetch('http://localhost:3030/data/recipes')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setRecipes(data);
+  //     })
+  // }, []);
 
-  // const recipes = [
-  //   { id: 1, title: "Recipe Title 1", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-  //   { id: 2, title: "Recipe Title 2", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-  //   { id: 3, title: "Recipe Title 3", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
-  //   { id: 4, title: "Recipe Title 4", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" }
-  // ]
+  const recipes = [
+    { id: 1, title: "Recipe Title 1", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 2, title: "Recipe Title 2", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 3, title: "Recipe Title 3", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" },
+    { id: 4, title: "Recipe Title 4", description: "Some fancy recipe description", likes: 14, img: "../images/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" }
+  ]
 
   return (
     <>
